7caf96c4f73590184065de5173536790c3b6e812
CWY <cwyoo01@naver.com>
2021-07-08 18:25:20 +0900

    210708 Finished Index

---
+++ main.js
@@ -0,0 +1,25 @@
+const http = require('http');
+const fs = require('fs');
+const url = require('url');
+const qs = require('querystring');
+const temp = require('./modules/template.js');
+
+
+var app = http.createServer((req, res) => {
+    // url 가져오기
+    const baseURL = 'http://' + req.headers.host + '/';
+    var _url = new URL(req.url, baseURL);
+    var queryData = _url.searchParams.get('id');
+    // 메인 홈페이지 방문
+    if (_url.href == baseURL) {
+        var title = '어떤게 고민이세요?';
+        var template = temp.getTemplate(title, '');
+        res.writeHead(200);
+        res.end(template);
+    }
+    else {
+        res.writeHead(404);
+        res.end('Not Legal access');
+    }
+});
+app.listen(3000);
---
+++ modules/template.js
@@ -0,0 +1,33 @@
+var operation = {
+        getTemplate: function(title, description) {
+        return `
+        <!DOCTYPE html>
+        <html lang="kr">
+        <head>
+            <meta charset="UTF-8">
+            <title>선택장애해결기</title>
+            <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+            <link rel="stylesheet" href="css/style.css">
+            <script src="random.js" type="text/javascript"></script>
+        </head>
+        <body>
+            <h1>${title}</h1>
+            <a href="/?id=eat"><button type="button" class="btn btn-secondary">음식완전랜덤뽑기</button></a>
+            <a href="/?id=lotto"><button type="button" class="btn btn-secondary">로또번호랜덤뽑기</button></a>
+            <p>고민되는 품목들을 열거해주세요</p>
+            <p>띄어쓰기로 각 품목들을 구분해주세요</p>
+            <p>EX) 자갈치 꼬깔콘 다이제 초코송이</p>
+            <form action="/?id=res" method="post">
+                <div class="mt-4 mx-auto search-bar input-group mb-3">
+                    <input name="id" type="text" class="form-control rounded-pill" placeholder="품목 입력" aria-label="Recipient's username" aria-describedby="button-addon2">
+                </div>
+                <input type="submit">
+            </form>
+            ${description}
+            </body>
+        </html>
+        `
+    }
+}
+
+module.exports = operation;
