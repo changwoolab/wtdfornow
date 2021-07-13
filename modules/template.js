36d5f98386b302e40897a38abd511bf9e9848b2a
CWY <cwyoo01@naver.com>
2021-07-13 23:42:01 +0900

    Designing... Do it More!

--- modules/template.js
+++ modules/template.js
@@ -13,33 +13,40 @@ var operation = {
             </style>
         </head>
         <body>
-            <div class="title">
-                <a href="/"><h1>선택장애해결기</h1></a><br>
-            </div>
-            <a href="/?id=eat"><button type="button" class="btn btn-secondary">음식완전랜덤뽑기</button></a>
-            <a href="/?id=lotto"><button type="button" class="btn btn-secondary">로또번호랜덤뽑기</button></a>
-            <p>고민되는 품목들을 열거해주세요</p>
-            <p>띄어쓰기로 각 품목들을 구분해주세요</p>
-            <p>EX) 자갈치 꼬깔콘 다이제 초코송이</p>
-            <form action="/res" method="post">
-                <div class="mt-4 mx-auto search-bar input-group mb-3">
-                    <input name="id" type="text" class="form-control rounded-pill" placeholder="품목 입력" aria-label="Recipient's username" aria-describedby="button-addon2">
+            <a href="/" class="title"><h1 class="title">선택장애해결기</h1></a><br>
+            <div class="container">
+                <a href="/?id=eat"><button type="button" class="btn btn-secondary">음식완전랜덤뽑기</button></a>
+                <a href="/?id=lotto"><button type="button" class="btn btn-secondary">로또번호랜덤뽑기</button></a>                
+                <p>고민되는 품목들을 열거해주세요</p>
+                <p>띄어쓰기로 각 품목들을 구분해주세요</p>
+                <p>EX) 자갈치 꼬깔콘 다이제 초코송이</p>
+                <form action="/res" method="post">
+                    <div class="mt-4 mx-auto search-bar input-group mb-3">
+                        <input name="id" type="text" class="form-control rounded-pill" placeholder="품목 입력" aria-label="Recipient's username" aria-describedby="button-addon2">
+                    </div>
+                    <div class="d-grid gap-2 col-6 mx-auto">
+                        <button class="btn btn-primary" type="submit">${retryOrTry}!</button>
+                    </div>
+                </form>
+                <br><br>
+                <h5>결과</h5>
+                <h1 class="des">${description}</h1>
+                <br><br><br><br><br><br>
+                <p>피드백 주기</p>
+                <form action="/feedback" method="post">
+                    <div>
+                        <input type="text" name="title" placeholder="피드백 제목">
+                    </div>
+                    <div>
+                        <textarea name="description" placeholder="피드백 내용"></textarea>
+                    </div>
+                        <input type="submit" value="제출">
+                </form>
                 </div>
-                <input type="submit" value=${retryOrTry}>
-            </form>
-            ${description}
-            <br><br><br><br><br><br>
-            <p>피드백 주기</p>
-            <form action="/feedback" method="post">
-                <div>
-                    <input type="text" name="title" placeholder="피드백 제목">
-                </div>
-                <div>
-                    <textarea name="description" placeholder="피드백 내용"></textarea>
-                </div>
-                    <input type="submit" value="제출">
-            </form>
-            </body>
+                <br><br><br><br>
+                <p>ChangWoo's First Website.</p>
+                <p>If you want to contact, please send an email to "cwyoo01@naver.com"</p>
+        </body>
         </html>
         `
     }
